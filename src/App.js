787b3482a6f780232a3dff8f3d8b6052ca54b13d
Trinidad Burdiak <burdiaktrinidad33@gmail.com>
2021-03-10 01:26:27 -0300

    commit clase 3 navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavBar} from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bali Planty Life
-        </p>
-        <p> Burdiak Trinidad </p>
-      
-      </header>
+      <NavBar/>
+      <p>Burdiak Trinidad</p>
+  
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+export const NavBar = () => {
+    return (
+        <div>
+            <h1>Bali Planty Life</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.html" >Home</a></li>
+                    <li><a href="shop.html" >Shop</a></li>
+                    <li><a href="contact.html" >Contact</a></li>
+                </ul>
+                
+                
+                
+            </nav>
+        </div>
+    )
+}
