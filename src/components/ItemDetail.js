3b786c2ef23f56a5878c82ddfaa1e3ea0d73f7b0
Trinidad Burdiak <burdiaktrinidad33@gmail.com>
2021-03-26 01:00:05 -0300

    a

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ItemDetail = ({ item}) => {
  
     return (
-        <div>
+        <div className="card">
             <img src={item.imagen} alt="" className="imagen"/>
             <h2>{item.titulo}</h2>
             <p>{item.descripcion}</p>
